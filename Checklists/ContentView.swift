ea6108661ef2f666421bd4b2a15cbc0989b4d95c
Hu <hu@pactinsurance.com>
2023-03-20 23:11:09 +0800

    Implement CRUD for List

--- Checklists/ContentView.swift
+++ Checklists/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var checkListItems = [
+        "Walk the dog",
+        "Brush my teeth",
+        "Learn iOS development",
+        "Soccer practice",
+        "Eat ice cream"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(checkListItems, id: \.self) { item in
+                    Text(item)
+                        .onTapGesture {
+                            let indexsToRemove = IndexSet(integersIn: 0...4)
+                            self.checkListItems.remove(atOffsets: indexsToRemove)
+//                            self.checkListItems.remove(at: 0)
+//                            self.checkListItems.append(item + "\(Int.random(in: 1...10))")
+                            self.printAll()
+                        }
+                }
+                .onDelete(perform: deleteListItem)
+                .onMove(perform: moveListItem)
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarItems(trailing: EditButton())
+            .navigationTitle("Checklist")
+            .onAppear() {
+                self.printAll()
+            }
         }
-        .padding()
+    }
+    
+    func printAll() {
+        for item in checkListItems {
+            print(item)
+        }
+    }
+    
+    func deleteListItem(whichElement: IndexSet) {
+        checkListItems.remove(atOffsets: whichElement)
+        printAll()
+    }
+    
+    func moveListItem(whichElement: IndexSet, destination: Int) {
+        checkListItems.move(fromOffsets: whichElement, toOffset: destination)
+        printAll()
     }
 }
 
