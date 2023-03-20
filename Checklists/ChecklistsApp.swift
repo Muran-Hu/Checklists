dd0bab1d4734198620bb790818faa958ed80aa0f
Hu <hu@pactinsurance.com>
2023-03-20 21:56:56 +0800

    Initial Commit

---
+++ Checklists/ChecklistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistsApp.swift
+//  Checklists
+//
+//  Created by Mac on 2023/3/20.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
